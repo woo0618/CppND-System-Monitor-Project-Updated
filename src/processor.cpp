a9298b91689e5792e270599ee5ea7d822d8ecf6a
woo0618 <37446787+woo0618@users.noreply.github.com>
2020-04-15 21:41:10 -0400

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,15 @@
 #include "processor.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+  const long prev_idle = LinuxParser::IdleJiffies();
+  const long pre_total = LinuxParser::Jiffies();
+  
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  long idle = LinuxParser::IdleJiffies();
+  long total = LinuxParser::Jiffies();
+  
+  long totald = total - pre_total;
+  long idled = idle - prev_idle;
+  return (float)(totald - idled) / totald;
+}
