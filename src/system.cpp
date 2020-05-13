94573807eb6df2e78cff0e2b352af115415461d9
woo0618 <37446787+woo0618@users.noreply.github.com>
2020-05-12 21:40:24 -0400

    Update system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -13,26 +13,34 @@ using std::size_t;
 using std::string;
 using std::vector;
 
-// TODO: Return the system's CPU
+// TODO: Return the system's CPU (DONE)
 Processor& System::Cpu() { return cpu_; }
 
-// TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+// TODO: Return a container composed of the system's processes (DONE)
+vector<Process>& System::Processes() {
+  vector<int> pids = LinuxParser::Pids();
+  for (size_t i = 0; i < pids.size(); i++) {
+    processes_.push_back(Process(pids[i]));
+  }
+  
+  std::sort(processes_.begin(), processes_.end(), [](Process const& a, Process const& b) { return a > b; });
+  return processes_;
+}
 
-// TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+// TODO: Return the system's kernel identifier (string) (DONE) 
+std::string System::Kernel() { return LinuxParser::Kernel();}
 
-// TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+// TODO: Return the system's memory utilization (DONE) 
+float System::MemoryUtilization() { return LinuxParser::MemoryUtilization();}
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// TODO: Return the operating system name (DONE)
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
-// TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+// TODO: Return the number of processes actively running on the system (DONE) 
+int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
-// TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+// TODO: Return the total number of processes on the system (DONE) 
+int System::TotalProcesses() { return LinuxParser::TotalProcesses();}
 
-// TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+// TODO: Return the number of seconds since the system started running (DONE) 
+long int System::UpTime() { return LinuxParser::UpTime(); }
